48a24f0b1090bb99307cbd93179b5f638c22b8f8
Raunak Singha <rksingha1212@gmail.com>
2019-06-26 22:49:52 +0530

    Resolved db connection error

--- db.js
+++ db.js
@@ -1,45 +1,22 @@
 import { Pool } from 'pg';
-import { Client } from 'pg';
 import keys from './config/keys';
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    const client = new Client({
-        connectionString: keys.DATABASE_URL,
-        ssl: true
-    });
+const pool = new Pool({
+    connectionString: keys.DATABASE_URL
+});
 
-    module.exports = {
-        query(text, params) {
-            return new Promise((resolve, reject) => {
-                client.query(text, params)
-                    .then((res) => {
-                        resolve(res);
-                    })
-                    .catch((err) => {
-                        reject(err);
-                    })
-            })
-        }
-    }
-
-} else {
-    const pool = new Pool({
-        connectionString: keys.DATABASE_URL
-    });
-
-    module.exports = {
-        query(text, params) {
-            return new Promise((resolve, reject) => {
-                pool.query(text, params)
-                    .then((res) => {
-                        resolve(res);
-                    })
-                    .catch((err) => {
-                        reject(err);
-                    })
-            })
-        }
+module.exports = {
+    query(text, params) {
+        return new Promise((resolve, reject) => {
+            pool.query(text, params)
+                .then((res) => {
+                    resolve(res);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
     }
 }
